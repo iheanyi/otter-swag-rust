b3871f1c8107ad4f0d97f2b3f1d88fe6ddd4d7e9
Iheanyi Ekechukwu <iheanyi@github.com>
2018-09-29 19:02:41 -0500

    Make some things private, add some tests, also add a little bit of helper functions.

--- models/menu.rs
+++ models/menu.rs
@@ -2,7 +2,7 @@ use sdl2::rect::Rect;
 
 const MENU_TILE_SIZE: (u32, u32) = (480, 320);
 
-#[derive(Copy, Clone)]
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
 pub enum MenuState {
     StartScreen { source_rect: Rect, is_visible: bool },
     Playing { source_rect: Rect, is_visible: bool },
@@ -36,10 +36,18 @@ impl MenuState {
             is_visible: true,
         };
     }
+
+    pub fn as_str(&self) -> &'static str {
+        match *self {
+            MenuState::StartScreen { .. } => "start",
+            MenuState::Playing { .. } => "playing",
+            MenuState::GameOver { .. } => "game_over",
+        }
+    }
 }
 
 pub struct Menu {
-    pub state: MenuState,
+    state: MenuState,
 }
 
 impl Menu {
@@ -47,6 +55,10 @@ impl Menu {
         return Menu {
             state: MenuState::new_start(),
         };
+    }
+
+    pub fn state(&self) -> MenuState {
+        return self.state;
     }
 
     pub fn to_start_screen(&mut self) {
@@ -84,5 +96,47 @@ impl Menu {
             | MenuState::Playing { is_visible, .. }
             | MenuState::GameOver { is_visible, .. } => return is_visible,
         };
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn setup() -> Menu {
+        return Menu::new();
+    }
+
+    #[test]
+    fn it_starts_in_start_screen_state() {
+        let menu = setup();
+        assert_eq!(menu.state, MenuState::new_start());
+    }
+
+    #[test]
+    fn it_transitions_from_to_playing_from_start() {
+        let mut menu = setup();
+        menu.to_playing();
+        assert_eq!(menu.state, MenuState::new_playing());
+    }
+
+    #[test]
+    #[should_panic]
+    fn it_panics_from_start_screen_to_game_over() {
+        let mut menu = setup();
+        menu.to_game_over();
+    }
+
+    #[test]
+    fn it_can_go_through_all_states() {
+        let mut menu = setup();
+
+        assert_eq!(menu.state, MenuState::new_start());
+        menu.to_playing();
+        assert_eq!(menu.state, MenuState::new_playing());
+        menu.to_game_over();
+        assert_eq!(menu.state, MenuState::new_game_over());
+        menu.to_start_screen();
+        assert_eq!(menu.state, MenuState::new_start());
     }
 }
